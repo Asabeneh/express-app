dc0dcd0ce989103d64faa987224ba1cd2f0b25e9
Asabeneh <asabeneh@gmail.com>
2020-03-25 16:01:32 +0200

    connected to mongodb

---
+++ res.js
@@ -0,0 +1,36 @@
+let students = [
+  {
+    firstName: 'Asab',
+    lastName: 'Yeta',
+    age: 250,
+    country: 'Finland',
+    skills: ['HTML', 'CSS', 'JS', 'React'],
+    bio: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias consectetur rerum facilis repellat, accusamus eius atque! A quas quisquam velit, dolorum mollitia, necessitatibus provident laboriosam, fuga reprehenderit perspiciatis eum!`
+  },
+  {
+    firstName: 'Atik',
+    lastName: 'Rhaman',
+    age: 25,
+    country: 'Finland',
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node'],
+    bio: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias consectetur rerum facilis repellat, accusamus eius atque! A quas quisquam velit, dolorum mollitia, necessitatibus provident laboriosam, fuga reprehenderit perspiciatis eum!`
+  },
+  {
+    firstName: 'Bibek',
+    lastName: 'Dhakal',
+    age: 21,
+    country: 'Finland',
+    skills: ['HTML', 'CSS', 'JS', 'React', 'MongoDB'],
+    bio: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias consectetur rerum facilis repellat, accusamus eius atque! A quas quisquam velit, dolorum mollitia, necessitatibus provident laboriosam, fuga reprehenderit perspiciatis eum!`
+  },
+  {
+    firstName: 'Arthur',
+    lastName: 'Arthur',
+    age: 25,
+    country: 'Finland',
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Redux'],
+    bio: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias consectetur rerum facilis repellat, accusamus eius atque! A quas quisquam velit, dolorum mollitia, necessitatibus provident laboriosam, fuga reprehenderit perspiciatis eum!`
+  }
+]
+
+// Student.insertMany(students)
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const fs = require('fs')
 const os = require('os')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const { showDateTime } = require('./my_modules/my_modules.js')
+const Student = require('./models/Student')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -14,10 +17,22 @@ const app = express()
 // PUT: UPDATING
 // DELETE: DELETING
 
+// connect mongodb with the server
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) return console.log(err)
+    console.log('The server is connnected to MongoDB database')
+  }
+)
+
 // We are setting our view engine, ejs
 
 app.set('view engine', 'ejs')
 // Middle ware
+
 app.use((req, res, next) => {
   const user = os.hostname
   const page = req.url
@@ -41,41 +56,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-let students = [
-  {
-    _id: 1,
-    firstName: 'Asab',
-    lastName: 'Yeta',
-    age: 250,
-    country: 'Finland',
-    skills: ['HTML', 'CSS', 'JS', 'React']
-  },
-  {
-    _id: 2,
-    firstName: 'Atik',
-    lastName: 'Rhaman',
-    age: 25,
-    country: 'Finland',
-    skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node']
-  },
-  {
-    _id: 3,
-    firstName: 'Bibek',
-    lastName: 'Dhakal',
-    age: 21,
-    country: 'Finland',
-    skills: ['HTML', 'CSS', 'JS', 'React', 'MongoDB']
-  },
-  {
-    _id: 4,
-    firstName: 'Arthur',
-    lastName: 'Arthur',
-    age: 25,
-    country: 'Finland',
-    skills: ['HTML', 'CSS', 'JS', 'React', 'Redux']
-  }
-]
-
 // the home route
 
 app.get('/', (req, res) => {
@@ -94,21 +74,19 @@ app.get('/contact', (req, res) => {
 
 // all students route, show route or the UI
 app.get('/students', (req, res) => {
-  const student = {
-    name: 'Asabeneh',
-    age: 250,
-    country: 'Finland',
-    skills: ['HTML', 'CSS', 'JS']
-  }
-
-  res.render('pages/students', { students })
+  Student.find({}, (err, students) => {
+    if (err) return res.status(404).send('Not found')
+    res.render('pages/students', { students })
+  })
 })
 
 // route for a signle student, show route or UI
 app.get('/students/:id', (req, res) => {
   const id = req.params.id
-  const student = students.find(st => st._id == id)
-  res.render('pages/student', { student })
+  Student.findOne({ _id: id }, (err, student) => {
+    if (err) return res.status(404).send('Not found')
+    res.render('pages/student', { student })
+  })
 })
 
 // A form route to add student, show or UI
@@ -119,64 +97,70 @@ app.get('/student/add', (req, res) => {
 // An edit for to update student data, show or UI
 app.get('/student/edit/:id', (req, res) => {
   const id = req.params.id
-  const student = students.find(st => st._id == id)
-  console.log(student)
-
-  res.render('pages/edit-student', { student })
+  Student.findOne({ _id: id }, (err, student) => {
+    if (err) return res.status(404).send(err)
+    res.render('pages/edit-student', { student })
+  })
 })
 
 // GET all students, an API or JSON
 app.get('/api/v1.0.0/students', (req, res) => {
-  res.send(students)
+  Student.find({}, (err, students) => {
+    if (err) return res.status(404).send('Not found')
+    res.json(students)
+  })
 })
 
 // GET a single student, an object
-app.get('/students/api/:id', (req, res) => {
+app.get('/api/v1.0.0/students/:id', (req, res) => {
   const id = req.params.id
-  const student = students.find(
-    st => st._id == id || st.firstName.toLowerCase() == id.toLowerCase()
-  )
-
-  if (student) {
+  Student.findOne({ _id: id }, (err, student) => {
+    if (err) return res.status(404).send('Not found')
     res.send(student)
-  } else {
-    res.send('Student with this id does not exist')
-  }
+  })
 })
 
 // adding student route, api
 app.post('/api/v.1.0/students', (req, res) => {
-  console.log(req.body)
-  const id = students.length + 1
   req.body.skills = req.body.skills.split(',')
-  req.body._id = id
-  students.push(req.body)
-  res.redirect('/students')
+  const newStudent = new Student(req.body)
+  newStudent.save(err => {
+    if (err) return res.status(404).send('Not found')
+    res.redirect('/students')
+  })
 })
 
 // edit path or route, api
 app.post('/api/v.1.0/students/:id/edit', (req, res) => {
-  const id = +req.params.id
-  students = students.map(st => {
-    if (st._id == id) {
-      req.body._id = id
-      return req.body
-    }
-    return st
+  const id = req.params.id
+  req.body.skills = req.body.skills.split(',')
+  const { firstName, lastName, age, country, bio, skills } = req.body
+  Student.findOne({ _id: id }, (err, student) => {
+    if (err) return res.status(404).send(err)
+    student.firstName = firstName
+    student.lastName = lastName
+    student.age = age
+    student.country = country
+    student.skills = skills
+    student.bio = bio
+
+    student.save(err => {
+      if (err) return res.status(404).send(err)
+      res.redirect('/students')
+    })
   })
-  res.redirect('/students')
 })
 
 // route to delete
 app.get('/api/v.1.0/students/:id/delete', (req, res) => {
   const id = req.params.id
-  students = students.filter(st => st._id != id)
-  res.redirect('/students')
+  Student.deleteOne({ _id: id }, (err, student) => {
+    if (err) return res.status(404).send(err)
+    res.redirect('/students')
+  })
 })
 
 // listen port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`)
 })
-
-
